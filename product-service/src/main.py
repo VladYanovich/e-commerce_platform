800fd2d5768c5a2bfd18b8f2daa26c8c56a371e4
Vlad Yanovich <vladislavyanovich2004@gmail.com>
2025-12-26 18:14:48 +0100

    - created config and modified main

--- product-service/src/main.py
+++ product-service/src/main.py
@@ -1,7 +1,13 @@
 import uvicorn
 from fastapi import FastAPI
+from src.core.config import settings
+from src.api.routes import products
 
 app = FastAPI()
+app.include_router(
+    products.router,
+    prefix=settings.api.prefix
+    )
 
 if __name__ == "__main__":
     uvicorn.run("src.main:app", reload=True)
