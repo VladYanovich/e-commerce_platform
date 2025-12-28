265b24fe55c5cc6fba63134bc03c91a7e66e4d92
Vlad Yanovich <vladislavyanovich2004@gmail.com>
2025-12-28 17:37:01 +0100

    - config read from env

--- product-service/src/core/config.py
+++ product-service/src/core/config.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
+from pathlib import Path
 from pydantic import PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 class RunConfig(BaseModel):
     host: str = "127.0.0.1"
@@ -11,14 +14,20 @@ class ApiPrefix(BaseModel):
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
-    echo: bool = False,
-    echo_pool: bool = False,
-    pool_size: int = 50,
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
     max_overflow: int = 10
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(BASE_DIR / ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__"
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
 
-settings: Settings = Settings()
+settings = Settings()
--- product-service/src/main.py
+++ product-service/src/main.py
@@ -1,9 +1,24 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 from src.core.config import settings
 from src.api.routes import products
+from src.db.db_helper import db_helper
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    print("dispose_engine")
+    await db_helper.dispose()
+
+app = FastAPI(
+    lifespan=lifespan
+)
+
 app.include_router(
     products.router,
     prefix=settings.api.prefix
