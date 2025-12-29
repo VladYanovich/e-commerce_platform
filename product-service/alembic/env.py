0e9c18085164adb89a437315d04d4b52a382acc3
Vlad Yanovich <vladislavyanovich2004@gmail.com>
2025-12-29 08:22:36 +0100

    - add tables and create alembic migration

--- product-service/alembic/env.py
+++ product-service/alembic/env.py
@@ -2,7 +2,7 @@ import asyncio
 from logging.config import fileConfig
 
 from src.core.config import settings
-from src.db.base import Base
+from src.db.models import Base
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
